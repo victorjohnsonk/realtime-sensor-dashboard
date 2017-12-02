a7f4754fc4a781baa214db33365068b2039455fe
victorjohnsonk <mail.victorjohnson@gmail.com>
2017-12-02 04:29:49 +0000

    add minimal Flask app with hello route

---
+++ app.py
@@ -0,0 +1,10 @@
+﻿from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello from Real-Time Sensor Dashboard!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
