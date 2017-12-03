90136c21f4aeb8806a27bf83a268473846778a46
victorjohnsonk <mail.victorjohnson@gmail.com>
2017-12-03 04:53:20 +0000

    integrate background OpenAQ data fetch into Flask app, thread used

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-﻿from flask import Flask
+﻿from flask import Flask, jsonify
+import threading
+import time
+from app.data_fetcher import fetch_latest
 
 app = Flask(__name__)
 
+latest_data = []
+
+def background_fetch():
+    global latest_data
+    while True:
+        try:
+            latest_data = fetch_latest(city="Los Angeles", parameter="pm25", limit=10)
+        except Exception as e:
+            print("Fetch error:", e)
+        time.sleep(30)
+
 @app.route('/')
 def index():
-    return 'Hello from Real-Time Sensor Dashboard!'
+    return 'Real-Time Sensor Dashboard running with background fetcher.'
+
+@app.route('/data')
+def data():
+    return jsonify(latest_data)
 
 if __name__ == '__main__':
+    thread = threading.Thread(target=background_fetch, daemon=True)
+    thread.start()
     app.run(debug=True)
