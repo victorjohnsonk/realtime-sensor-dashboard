ce1b91697d995b6d3bba2af6ea3000b78355d1d7
victorjohnsonk <mail.victorjohnson@gmail.com>
2017-12-04 02:27:28 +0000

    implement server-side logic for dynamic city and pollutant filtering

--- app.py
+++ app.py
@@ -9,12 +9,17 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 latest_data = []
+active_filters = {"city": "Los Angeles", "parameter": "pm25"}
 
 def background_fetch():
     global latest_data
     while True:
         try:
-            new_data = fetch_latest(city="Los Angeles", parameter="pm25", limit=10)
+            new_data = fetch_latest(
+                city=active_filters["city"],
+                parameters=[active_filters["parameter"]],
+                limit=10
+            )
             if new_data != latest_data:
                 latest_data = new_data
                 socketio.emit('new_data', latest_data, broadcast=True)
@@ -30,10 +35,14 @@ def index():
 def data():
     return jsonify(latest_data)
 
-@app.route('/eda')
-def eda():
-    return render_template('eda.html')
-
+@socketio.on('filter_change')
+def handle_filter_change(msg):
+    city = msg.get('city', 'Los Angeles')
+    param = msg.get('parameter', 'pm25')
+    active_filters["city"] = city
+    active_filters["parameter"] = param
+    print("Filter updated:", active_filters)
+    emit('status', {'message': 'Filters updated'}, broadcast=False)
 
 if __name__ == '__main__':
     thread = threading.Thread(target=background_fetch, daemon=True)
